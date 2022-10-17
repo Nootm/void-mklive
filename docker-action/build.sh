cca278583ed6e8cde374843b8c509522c8762e54
Tyrek Chou <me@mw.ax>
2022-10-17 23:24:29 +0200

    Update build.sh

--- docker-action/build.sh
+++ docker-action/build.sh
@@ -3,7 +3,7 @@
 xbps-install -Sy xbps
 xbps-install -Syu
 xbps-install -Syu
-xbps-install -Sy make git
+xbps-install -Sy make git bash
 git clone https://github.com/Nootm/void-mklive.git
 make
 ./mklive.sh -a x86_64-musl -l en_GB.UTF-8 -o x.iso
