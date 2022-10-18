0ec3cc478164d6807eabb0704247d65f587b26c4
Tyrek Chou <me@mw.ax>
2022-10-18 06:21:07 +0200

    Update build.sh

--- docker-action/build.sh
+++ docker-action/build.sh
@@ -5,6 +5,5 @@ xbps-install -Syu
 xbps-install -Syu
 xbps-install -Sy make git bash
 git clone https://github.com/Nootm/void-mklive.git
-make
-./mklive.sh -a x86_64-musl -l en_GB.UTF-8 -o x.iso
+./mklive.sh -o x.iso -a x86_64-musl -l en_GB.UTF-8 -i gzip -s gzip -C "ipv6.disable=1"
 cp ./x.iso /github/workspace/
